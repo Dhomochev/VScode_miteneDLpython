64d96f8702262a8c72692699a60757fba7ea2561
Shio_Inoue <puyopuyo117117puyopuyo117117@gmail.com>
2025-09-24 12:41:55 +0900

    local フォルダの場所間違えた。なおす。

--- mitene-DL_v0_0.py
+++ mitene-DL_v0_0.py
@@ -1,3 +1,30 @@
+#   2025/09/21   v0_0
+# - module install : requests, beautifulsoup4
+#
+#  https://mitene.us/f/QkjREtbKY5M
+#  mitene-DL-v0_0.py
+
+import os
+import requests
+import json
+import re
+import time
+from bs4 import BeautifulSoup
+from datetime import datetime
+
+dl_dir = 'dl'
+os.makedirs(dl_dir, exist_ok=True)
+
+
+
+
+
+
+if __name__ == "__main__":
+    url = "https://mitene.us/f/QkjREtbKY5M"  # アルバムのURL
+    save_files(url, 1)  # 初回の呼び出し
+
+
 
 
 
